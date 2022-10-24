4c18aff34a54d444f9caf93b7e357b1d35ceb63e
john_william <johnr@lavameapp.com>
2022-10-23 21:16:14 -0500

    commit inicial

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = process.env.PORT;
+
+//handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//middleware para servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    nombre: 'JOHN WILLIAM REYES',
+    titulo: 'Curso de node',
+  });
+});
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    nombre: 'JOHN WILLIAM REYES',
+    titulo: 'Curso de node',
+  });
+});
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    nombre: 'JOHN WILLIAM REYES',
+    titulo: 'Curso de node',
+  });
+});
+app.get('*', (req, res) => {
+  res.send('404 | Page Not Found');
+});
+app.listen(port);
